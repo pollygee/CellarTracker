62355aa9384fdcd2b779c2c4b0573581449d1999
Polly Sileo <pollysileo@gmail.com>
2018-03-28 21:00:15 -0400

    Add controller and view for wines index

--- db/migrate/20180325224907_create_wines.rb
+++ db/migrate/20180325224907_create_wines.rb
@@ -1,7 +1,12 @@
 class CreateWines < ActiveRecord::Migration[5.1]
   def change
     create_table :wines do |t|
-
+      t.string :vineyard
+      t.string :varietal
+      t.string :description
+      t.integer :year
+      t.string :color
+      t.text :notes
       t.timestamps
     end
   end
